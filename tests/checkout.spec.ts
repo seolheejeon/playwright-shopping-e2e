4645b76d02a0796e16cbbd6c634c3336f8a865d4
seolheejeon <cjs8821@naver.com>
2026-01-26 21:51:54 +0900

    refactor: apply POM to checkout flow

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,35 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { ProductsPage } from "../pages/ProductsPage";
+import { CheckoutPage } from "../pages/CheckoutPage";
+import { USERS } from "../fixtures/credentials";
 
-test('[E2E] Checkout - completes after adding item', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+test("Checkout completes after adding item", async ({ page }) => {
+  const login = new LoginPage(page);
+  const products = new ProductsPage(page);
+  const checkout = new CheckoutPage(page);
 
-  // login
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await expect(page.locator('.title')).toHaveText('Products');
+  await login.goto();
+  await login.login(USERS.standard.username, USERS.standard.password);
+  await products.assertOnProducts();
 
-  // add item
-  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+  await products.addItemToCartByTestId();
+  await products.goToCart();
 
-  // go to cart
-  await page.locator('.shopping_cart_link').click();
-  await expect(page.locator('.title')).toHaveText('Your Cart');
-
-  // checkout
-  await page.locator('[data-test="checkout"]').click();
-  await expect(page.locator('.title')).toHaveText('Checkout: Your Information');
-
-  // fill info
-  await page.locator('[data-test="firstName"]').fill('Seolhee');
-  await page.locator('[data-test="lastName"]').fill('Kim');
-  await page.locator('[data-test="postalCode"]').fill('12345');
-  await page.locator('[data-test="continue"]').click();
-  await expect(page.locator('.title')).toHaveText('Checkout: Overview');
-
-  // finish
-  await page.locator('[data-test="finish"]').click();
-  await expect(page.locator('.title')).toHaveText('Checkout: Complete!');
-  await expect(page.locator('.complete-header')).toContainText('Thank you');
+  await checkout.startCheckout();
+  await checkout.fillInfo("Seolhee", "Jeon", "12345");
+  await checkout.finish();
+  await checkout.assertComplete();
 });
